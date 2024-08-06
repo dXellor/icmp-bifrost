18471a7769905c944b3faf152d997d9b45487ca8
N. Simic <nikola7simic@gmail.com>
2024-08-06 11:18:21 +0200

    fix: inherit enum classes from int-enum

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -1,11 +1,11 @@
-from enum import Enum
+from enum import IntEnum
 
-class ExitCodes(Enum):
+class ExitCodes(IntEnum):
     SUCCESS = 0
     INVALID_ARGUMENTS = 1
     UNRECOGNIZED_ARGUMENTS = 2
     CANT_OPEN_SOCKET = 3
 
-class Marks(Enum):
+class Marks(IntEnum):
     SENDING = 7130
     RECEIVING = 7131
