2d374780b90621afa4d28845a4be1dbd54f6e3a7
Nikola Simic <nikola7simic@gmail.com>
2024-08-08 14:54:55 +0200

    fix: iptables rules to match packet flow

--- src/scripts/setup_iptables_client.sh
+++ src/scripts/setup_iptables_client.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then    
-    echo "No server ip address supplied";
+if [ $# -ne 2 ]; then
+    echo "No arguments provided: client_ip, server_ip";
     exit -1;
 fi
 
-sudo iptables -t mangle -I PREROUTING -s 0.0.0.0/0 -j MARK --set-mark 7130;
-sudo iptables -t mangle -I INPUT -s 0.0.0.0/0 -j NFQUEUE --queue-num 1;
+MARK_TO_SERVER=7130
+MARK_FROM_SERVER=7131
+CLIENT_IP=$1
+SERVER_IP=$2
 
-# sudo iptables -t mangle -I PREROUTING -s $1 -p icmp -j MARK --set-mark 7131
-# sudo iptables -t mangle -I INPUT -s $1 -p icmp -j NFQUEUE --queue-num 1
+sudo iptables -t mangle -I OUTPUT -s $CLIENT_IP ! -p icmp -j MARK --set-mark $MARK_TO_SERVER;
+sudo iptables -t mangle -I POSTROUTING -s $CLIENT_IP -j NFQUEUE --queue-num 1;
+
+sudo iptables -t mangle -I PREROUTING -s $SERVER_IP -p icmp -j MARK --set-mark $MARK_FROM_SERVER
+sudo iptables -t mangle -I INPUT -s $SERVER_IP -p icmp -j NFQUEUE --queue-num 1
--- src/scripts/setup_iptables_server.sh
+++ src/scripts/setup_iptables_server.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "No client ip address supplied";
+if [ $# -ne 1 ]; then
+    echo "No arguments provided: client_ip";
     exit -1;
 fi
 
-sudo iptables -t mangle -I PREROUTING -s $1 -p icmp -j MARK --set-mark 7132
-sudo iptables -t mangle -I INPUT -s $1 -p icmp -j NFQUEUE --queue-num 1
+MARK_FROM_CLIENT=7132
+MARK_TO_CLIENT=7133
+CLIENT_IP=$1
 
-# sudo iptables -t mangle -I PREROUTING -s 0.0.0.0/0 -j MARK --set-mark 7133;
-# sudo iptables -t mangle -I INPUT -s 0.0.0.0/0 -j NFQUEUE --queue-num 1;
+sudo iptables -t mangle -I PREROUTING -s $CLIENT_IP -p icmp -j MARK --set-mark $MARK_FROM_CLIENT
+sudo iptables -t mangle -I INPUT -s $CLIENT_IP -p icmp -j NFQUEUE --queue-num 1
+
+sudo iptables -t mangle -I PREROUTING ! -s $CLIENT_IP ! -d $CLIENT_IP -j MARK --set-mark $MARK_TO_CLIENT;
+sudo iptables -t mangle -I INPUT ! -s $CLIENT_IP ! -d $CLIENT_IP -j NFQUEUE --queue-num 1;
